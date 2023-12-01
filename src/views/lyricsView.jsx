f0b9bd1f7a8182044d86fd79926ad337627bbf66
malfo <malfo@kth.se>
2023-12-01 13:55:30 +0100

    make guess button added

--- src/views/lyricsView.jsx
+++ src/views/lyricsView.jsx
@@ -1,9 +1,9 @@
-// lyricsView.jsx
 import React, { useState } from 'react';
 
 function LyricsView({ title, lyrics }) {
   // State to keep track of guessed words
   const [guessedWords, setGuessedWords] = useState([]);
+  const [currentGuess, setCurrentGuess] = useState('');
 
   // Function to replace words with empty boxes
   const replaceWithEmptyBoxes = (text) => {
@@ -13,20 +13,27 @@ function LyricsView({ title, lyrics }) {
   };
 
   // Function to handle user guesses
-  const handleGuess = (guess) => {
-    // Add the guessed word to the state
-    setGuessedWords([...guessedWords, guess]);
+  const handleGuess = () => {
+    if (currentGuess.trim() !== '') {
+      // Add the guessed word to the state
+      setGuessedWords([...guessedWords, currentGuess.trim()]);
+      setCurrentGuess(''); // Clear the input after guessing
+    }
   };
 
   return (
     <div className="lyrics-view">
       <h2>{replaceWithEmptyBoxes(title)}</h2>
       <p>{replaceWithEmptyBoxes(lyrics)}</p>
-      <input
-        type="text"
-        placeholder="Make your guess"
-        onChange={(e) => handleGuess(e.target.value)}
-      />
+      <div>
+        <input
+          type="text"
+          placeholder="Type in your guess.."
+          value={currentGuess}
+          onChange={(e) => setCurrentGuess(e.target.value)}
+        />
+        <button onClick={handleGuess}>Guess</button>
+      </div>
     </div>
   );
 }
