957805ec8172607f252b91aaa9eea8fa8603bc47
minhaoni <minhaon@kth.se>
2023-12-04 13:05:54 +0100

    Issue#1: Add styles for hamburgerView

--- src/views/hamburgerView.jsx
+++ src/views/hamburgerView.jsx
@@ -3,15 +3,34 @@ import React from 'react';
 
 function HamburgerView() {
   return (
-    <div className="top-bar">
+    // <div className="top-bar">
+    //   <span className="game-title">Lyractle</span>
+    //   <span className="menu">
+    //     <select>
+    //       <option value="logout">Log Out</option>
+    //       <option value="mainMenu">Back to Menu</option>
+    //     </select>
+    //   </span>
+    // </div>
+
+    <div class="navbar">
+
+
       <span className="game-title">Lyractle</span>
-      <span className="menu">
-        <select>
-          <option value="logout">Log Out</option>
-          <option value="mainMenu">Back to Menu</option>
-        </select>
-      </span>
-    </div>
+      
+      <ul class="nav-list">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+      </ul>
+
+      <div class="hamburger-menu">
+          <div class="bar"></div>
+          <div class="bar"></div>
+          <div class="bar"></div>
+      </div>
+  </div>
   );
 }
 
