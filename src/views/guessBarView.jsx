267f939a20a4d40a6bde117b64fe235065e4250a
minhaoni <minhaon@kth.se>
2023-12-08 10:25:44 +0100

    Fix: The rendering of user guesses is adjusted

--- src/views/guessBarView.jsx
+++ src/views/guessBarView.jsx
@@ -7,10 +7,22 @@ function GuessBarView(props) {
   return (
     <div className="guess-bar-view">
       {/* Display the list of guessed words and the number of occurrences */}
-      <ul className="guessed-words">
+      <h2 className="guess-title">Your Guesses: </h2>
+      {/* <ul className="guessed-words">
         {props.guesses.map(showGuessCB)}
-        {/* Add more guessed words as needed */}
-      </ul>
+      </ul> */}
+
+      <table>
+        <tr>
+            <th>#</th>
+            <th>Guess</th>
+            <th>Hits</th>
+        </tr>
+        {props.guesses.map(showGuessCB)}
+
+      </table>
+
+
       {/* Include buttons for showing hints and quitting the game */}
       <div className="guess-bar-button-container">
         <button disabled={props.hints>=3} className='hint-button' onClick={getHintACB}>Hints {props.hints}/3</button>
@@ -29,7 +41,13 @@ function GuessBarView(props) {
 
   function showGuessCB(guess, index) {
     // TODO: Write nbr of occurrences
-    return <li className="user-guesses" key={index}>{guess}: {props.currentOccurence} occurrences</li>
+    // return <li className="user-guesses" key={index}>{guess}: {props.currentOccurence} occurrences</li>
+    return (<tr>
+              <th>{index}</th>
+              <th>{guess}</th>
+              <th>{props.currentOccurence}</th>
+            </tr>
+    )
 
   }
 
