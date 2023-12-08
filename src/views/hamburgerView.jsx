01807bc157aa4cd6db5a68219b7304bb0e310ebf
minhaoni <minhaon@kth.se>
2023-12-08 14:08:57 +0100

    Fix: Change the style of dropdown menu

--- src/views/hamburgerView.jsx
+++ src/views/hamburgerView.jsx
@@ -13,12 +13,13 @@ function HamburgerView(props) {
         <div className="bar"></div>
         <div className="bar"></div>
       </div>
-
+ 
       <ul className="nav-list">
-        <li><button onClick={mainMenuACB}>main menu</button></li>
-        <li><button onClick={highScoresACB}>high scores</button></li>
-        <li><button onClick={logoutACB}>log out</button></li>
-      </ul>
+        <li><button className='dropdown-button' onClick={mainMenuACB}>main menu</button></li>
+        <li><button className='dropdown-button' onClick={highScoresACB}>high scores</button></li>
+        <li><button className='dropdown-button' onClick={logoutACB}>log out</button></li>
+      </ul> 
+
     </div>
   )
 
