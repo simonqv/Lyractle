7aa14d0e826233e396a5c47de8c4373904f7d2ea
malfo <malfo@kth.se>
2023-12-14 13:41:04 +0100

    new style for searchbar

--- src/views/guessInputView.jsx
+++ src/views/guessInputView.jsx
@@ -1,32 +1,39 @@
+import React from 'react';
 import '/src/style.css';
 
-
 function GuessInputView(props) {
+  
+  const handleKeyPress = (event) => {
+      if (event.key === 'Enter') {
+        handleGuessACB();
+      }
+    };
   return (
-
     <div className="textarea-container">
+      <div className="search-bar-container">
         <textarea
           className="search-bar"
           placeholder="Write your guess..."
           value={props.currentGuess}
           onChange={setCurrentGuessACB}
-          ></textarea>
-
-        <div className="guess-input-button-container">
-          <button className="guess-button" onClick={handleGuessACB}>
-              Guess
+          onKeyPress={handleKeyPress} 
+        ></textarea>
+         <div className="button-container">
+          <button className="button small-button" onClick={handleGuessACB}>
+            Guess
           </button>
         </div>
-        
+      </div>
     </div>
-  )
-    function handleGuessACB() {
-        props.onHandleGuess()
-    }
+  );
 
-    function setCurrentGuessACB(evt) {
-      props.onSetGurrentGuess(evt.target.value)
-    }
+  function handleGuessACB() {
+    props.onHandleGuess();
+  }
+
+  function setCurrentGuessACB(evt) {
+    props.onSetGurrentGuess(evt.target.value);
+  }
 }
 
-export default GuessInputView
+export default GuessInputView;
